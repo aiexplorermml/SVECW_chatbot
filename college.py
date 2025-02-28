5ecba7b546b5b5f3fa1e3fbf20b3980613448169
aiexplorermml <156586900+aiexplorermml@users.noreply.github.com>
2025-02-28 11:43:46 +0530

    Update college.py

--- college.py
+++ college.py
@@ -3,7 +3,7 @@ import pandas as pd
 import google.generativeai as genai
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
-st.set.set_page_config(page_title="Svecw College Chatbot",layout="centered")
+st.set_page_config(page_title="Svecw College Chatbot",layout="centered")
 if "messages" not in st.session_state:
   st.session_state.messages=[]
 csv_url="svecw_details.csv"
