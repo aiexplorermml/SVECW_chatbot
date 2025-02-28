82b2877d4deeb5dc4c7e90f4496c849ca2701753
aiexplorermml <156586900+aiexplorermml@users.noreply.github.com>
2025-02-28 11:09:32 +0530

    Update college.py

--- college.py
+++ college.py
@@ -25,7 +25,7 @@ def find_closest_question(user_query,vectorizer,question_vectors,df):
   query_vector=vectorizer.transform([user_query,lower()])
   similarities=cosine_similarity(query_vector,question_vectors).flatten()
   best_match_index=similarities.argmax()
-  best_matc_score=similarities[best_match_index]
+  best_match_score=similarities[best_match_index]
   if best_match_score>0.3:
     return df.iloc[best_match_index]['Answer']
   else:
@@ -35,3 +35,23 @@ st.write("Welcome to the college ChatBot! Ask me Anything about the college.")
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+if prompt := st.chat_input("Say something..."):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    closest_answer=find_cloasest_question(prompt,vectorizer,question_vectors,df)
+    if closest_answer:
+      st.session_state.messages.append({"role":"assistant","content":closest_answer})
+      with st.chat_message("assistant"):
+        st.markdown(closest_answer)
+    else:
+      try:
+        response=model.generate_content(prompt)
+        st.session_state.messages.append({"role":"assistant","content":response.text})
+        with st.chat_message("assistant"):
+          st.markdown(response.text)
+      except Exception as e:
+        st.error(f"Sorry, I couldn't generate a response.Error: {e}")
+        
+      
